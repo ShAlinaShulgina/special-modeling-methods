c2613a10dfac9de8a4e524390d28ff3030fe4256
ShAlinaShulgina <shulginaali@yandex.ru>
2020-12-10 17:17:24 +0300

    Update files on Lab1

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -1,60 +1,59 @@
-#include "common.h"
+#include <iostream>
+#include <iomanip>      // std::setw
+#include <cmath>        // exp
+#include <string.h>
+#include <fstream>
 
 using namespace std;
 
 bool isCout;
 
-class Euler : public Common
+float dxdy(float x, float y)
 {
-public:
-    Euler();
-    void getResult(float);
-};
-
-class ModEuler : public Common
-{
-public:
-    ModEuler();
-    void getResult(float);
-};
-
-class RK: public Common
-{
-public:
-    RK();
-    void getResult(float);
-};
-
-Euler::Euler()
-{
-    file_name = "Euler.dat";
-    openFile();
+    return (exp(x) - 1);
 }
 
-void Euler::getResult(float h)
+FILE* openFile(std::string name)
 {
+    FILE* file = fopen(name.c_str(), "w");
+    if (file == NULL)
+    {
+        perror("Error to open file");
+        return NULL;
+    }
+    return file;
+}
+
+void addDataOnFile(FILE *f, float x, float y)
+{
+    fprintf(f,"%f\t%f\n", static_cast<double>(x), static_cast<double>(y));
+}
+
+void closeFile(FILE *f)
+{
+    fclose(f);
+}
+
+float Euler(float x_0, float y_0, float x_n, float h)
+{
+    FILE *file = openFile("Euler.dat");
     float x = x_0, y = y_0;
     float n = (x_n - x_0) / h;
     for (int i = 1; i <= n; i++)
     {
         x = x_0 + i * h;
         y = y + h * dxdy(x, y);
-        addDataOnFile(x, y);
+        addDataOnFile(file, x, y);
         if (::isCout) cout << "step=" << i <<
                 " x=" << x << " y=" << y << endl;
     }
-    closeFile();
-    cout << y << endl;
+    closeFile(file);
+    return y;
 }
 
-ModEuler::ModEuler()
+float modifiedEuler(float x_0, float y_0, float x_n, float h)
 {
-    file_name = "modifiedEuler.dat";
-    openFile();
-}
-
-void ModEuler::getResult(float h)
-{
+    FILE *file = openFile("modifiedEuler.dat");
     float x = x_0, y = y_0, _x = x;
     float n = (x_n - x_0) / h;
     float _y;
@@ -64,22 +63,17 @@ void ModEuler::getResult(float h)
         x = x_0 + i * h;
         y = y + h / 2 * (dxdy(_x, y) + dxdy(x, _y));
         _x = x;
-        addDataOnFile(x, y);
+        addDataOnFile(file, x, y);
         if (::isCout) cout << "step=" << i <<
                 " x=" << x << " y=" << y << endl;
     }
-    closeFile();
-    cout << y << endl;
+    closeFile(file);
+    return y;
 }
 
-RK::RK()
+float RK(float x_0, float y_0, float x_n, float h)
 {
-    file_name = "RK.dat";
-    openFile();
-}
-
-void RK::getResult(float h)
-{
+    FILE *file = openFile("RK.dat");
     float k1 = 0.f, k2 = 0.f, k3 = 0.f, k4 = 0.f;
     float x = x_0, y = y_0;
     float n = (x_n - x_0) / h;
@@ -92,59 +86,43 @@ void RK::getResult(float h)
 
         y = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
         x = x_0 + i * h;
-        addDataOnFile(x, y);
+        addDataOnFile(file, x, y);
         if (::isCout) cout << "step=" << i <<
                 " x=" << x << " y=" << y << endl;
     }
-    closeFile();
-    cout << y << endl;
+    closeFile(file);
+    return y;
 }
 
 
 int main(int argc, char *argv[])
 {
+    float h1 = 0.01f, h2 = 0.001f;
+    float x_0 = 0, x_n = 1;
+    float y_0 = 0;
+
     if (argc > 1)
     {
-        if (!(strcmp(argv[2], "-1")))
+        if (!(strcmp(argv[1], "-1")))
             ::isCout = true;
         else
             ::isCout = false;
-        if (!(strcmp(argv[1], "-e")))
-        {
-            cout << "Метод Эйлера" << endl;
-            Euler* euler = new Euler();
-            euler->getResult(euler->h1);
-            euler->draw();
-            euler->getResult(euler->h2);
-            euler->draw();
-            std::cin.get();
-        }
-        else if (!(strcmp(argv[1], "-me")))
-        {
-            cout << "Модифицированный метод Эйлера" << endl;
-            ModEuler *meuler = new ModEuler();
-            meuler->getResult(meuler->h1);
-            meuler->draw();
-            meuler->getResult(meuler->h2);
-            meuler->draw();
-            std::cin.get();
-        }
-        else if (!(strcmp(argv[1], "-rk")))
-        {
-            cout << "Метода Рунге-Кутты четвертого порядка" << endl;
-            RK *rk = new RK();
-            rk->getResult(rk->h1);
-            rk->draw();
-            rk->getResult(rk->h2);
-            rk->draw();
-            std::cin.get();
-        }
-        else
-        {
-            cout << "Wasn't choose method" << endl;
-        }
     }
     else
-       ::isCout = false;
+        ::isCout = false;
+
+
+    cout << "Метод Эйлера" << endl;
+    cout << "h=" << h1 << " " << Euler(x_0, y_0, x_n, h1) << endl;
+    cout << "h=" << h2 << " " << Euler(x_0, y_0, x_n, h2) << endl;
+
+    cout << "Модифицированный метод Эйлера" << endl;
+    cout << "h=" << h1 << " " << modifiedEuler(x_0, y_0, x_n, h1) << endl;
+    cout << "h=" << h2 << " " << modifiedEuler(x_0, y_0, x_n, h2) << endl;
+
+    cout << "Метода Рунге-Кутты четвертого порядка" << endl;
+    cout << "h=" << h1 << " " << RK(x_0, y_0, x_n, h1) << endl;
+    cout << "h=" << h2 << " " << RK(x_0, y_0, x_n, h2) << endl;
+
     return 0;
 }
